b04ba6c1629d28c14a75606f425ecb23f232f58f
boyantodorov <boyanto95@gmail.com>
2022-04-14 18:41:53 +0200

    move requirements.txt

--- steal_destination/settings.py
+++ steal_destination/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
