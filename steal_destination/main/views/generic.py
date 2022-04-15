ddb0f13694163bc9c52b8575d5b37bc6fd8cdb88
boyantodorov <boyanto95@gmail.com>
2022-04-15 10:07:59 +0200

    changes

--- steal_destination/main/views/generic.py
+++ steal_destination/main/views/generic.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views import generic as views
 
-
 from steal_destination.main.models import Destination
 
 UserModel = get_user_model()
@@ -17,7 +16,8 @@ class HomePageView(views.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['first_six_destinations'] = self.object_list[:6]
+        if self.object_list:
+            context['first_destination'] = self.object_list[0]
+        # if self.object_list[1:6]:
+        context['second_to__sixth_destinations'] = self.object_list[1:6]
         return context
-
-
